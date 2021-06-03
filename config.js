8fa5396ef72971bd9d7efd81d56d25b23250b0ed
vanly0911 <85274313+vanly0911@users.noreply.github.com>
2021-06-03 22:24:03 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+  
+const CONFIG = {
+    introTitle: 'Kiều à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'haha',
+    title: 'Một câu hỏi hơi tế nhị 😙',
+    desc: 'Phải chăng em bị khùng lâu rồi phải hk ',
+    btnYes: 'uk, đúng ròi. Bị lâu rồi mà hk dám nói',
+    btnNo: 'Không, làm gì có :3',
+    question:'rên thế giới hơn 7 tỉ người mà sao em bị khùng vậy <3',
+    btnReply: 'Gửi bệnh viện',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'tôi biết mà 🥰.chúng tôi đã cố gắng hết sức 😘😘',
+    messDesc: 'bạn có đồng ý căn bệnh của mình hk',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
